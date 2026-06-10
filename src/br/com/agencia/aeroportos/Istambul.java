bafa4dda1b8d16538e4602fe169c8acf78f17723
jap4h <jap4hhh@gmail.com>
2026-06-10 20:14:13 -0300

    Istambull

---
+++ src/br/com/agencia/aeroportos/Istambul.java
@@ -0,0 +1,25 @@
+import br.com.agencia.aeroportos.Aeroporto;
+public class Istambul implements Aeroporto{
+    public Istambul(){
+    }
+
+    @Override
+    public String obterNomeAeroporto() {
+        return "Aeroporto Istanbul-Airport (IST)";
+    }
+
+    @Override
+    public double calcularPrecoVoo(int quantidadePassageiros) {
+        double taxaEmbarqueEUA = 250.00;
+        double precoPassagemBase = 6.020;
+
+        // Custo total baseado nos passageiros
+        return (precoPassagemBase + taxaEmbarqueEUA) * quantidadePassageiros;
+    }
+
+    @Override
+    public int obterTempoVooMinutos() {
+        // Um voo de SP para LUKLA dura cerca de 25 horas e 0 minutos (1500 minutos)
+        return 360;
+    }
+}
