42b271653a6a809381cb59a206c9b3a808e2ad77
matheus-lacerda009 <matheuslacerda.mala@gmail.com>
2026-06-10 19:55:45 -0300

    Adição: Adicionando aeroporto JFK

---
+++ src/br/com/agencia/aeroportos/NovaYork.java
@@ -0,0 +1,23 @@
+package br.com.agencia.aeroportos;
+
+public class NovaYork implements Aeroporto{
+    @Override
+    public String obterNomeAeroporto() {
+        return "Aeroporto Internacional John F. Kennedy (JFK)";
+    }
+
+    @Override
+    public double calcularPrecoVoo(int quantidadePassageiros) {
+        double taxaEmbarqueEUA = 250.00;
+        double precoPassagemBase = 3200.00;
+
+        // Custo total baseado nos passageiros
+        return (precoPassagemBase + taxaEmbarqueEUA) * quantidadePassageiros;
+    }
+
+    @Override
+    public int obterTempoVooMinutos() {
+        // Um voo de SP para LUKLA dura cerca de 25 horas e 0 minutos (1500 minutos)
+        return 575;
+    }
+}
