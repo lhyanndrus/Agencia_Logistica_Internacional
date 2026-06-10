f2e86b4f012531dad637dde49eb56e40940a2dd9
Mafioletti <leomafioletti@gmail.com>
2026-06-10 19:47:05 -0300

    New Feature LosAngeles - LAX

---
+++ src/br/com/agencia/aeroportos/LosAngeles.java
@@ -0,0 +1,24 @@
+package br.com.agencia.aeroportos;
+
+public class LosAngeles implements Aeroporto {
+
+    @Override
+    public String obterNomeAeroporto() {
+        return "Aeroporto LosAngeles (LAX)";
+    }
+
+    @Override
+    public double calcularPrecoVoo(int quantidadePassageiros) {
+        double taxaEmbarqueEUA = 300.00;
+        double precoPassagemBase = 4500.00;
+
+        // Custo total baseado nos passageiros
+        return (precoPassagemBase + taxaEmbarqueEUA) * quantidadePassageiros;
+    }
+
+    @Override
+    public int obterTempoVooMinutos() {
+        // Um voo de SP para LUKLA dura cerca de 25 horas e 0 minutos (1500 minutos)
+        return 960;
+    }
+}
